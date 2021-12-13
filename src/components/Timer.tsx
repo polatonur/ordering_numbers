70009a9396c700fb6cb155e17facbea844a0eb7f
onur <onurpolatplt@gmail.com>
2021-12-13 08:13:19 +0100

    add some styles to timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useLevel } from "../context/LevelContext";
+
+interface clockProps {
+  status: string;
+}
 
 const Container = styled.div`
   display: flex;
@@ -7,11 +12,16 @@ const Container = styled.div`
   justify-content: center;
   margin-top: 50px;
 `;
-const Clock = styled.div`
+const Clock = styled.div<clockProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: lightgreen;
+  background-color: ${(props) =>
+    props.status === "Alert"
+      ? "tomato"
+      : props.status === "Finished"
+      ? "red"
+      : "lightgreen"};
   width: 150px;
   height: 150px;
   border: 2px solid black;
@@ -20,9 +30,38 @@ const Clock = styled.div`
 `;
 
 const Timer = () => {
+  const {
+    state: { level },
+  } = useLevel();
+
+  //   Calculate timer
+  const initialTime = () => {
+    if (level === "EASY") {
+      return 60;
+    } else if (level === "MEDIUM") {
+      return 30;
+    } else {
+      return 10;
+    }
+  };
+  const [timer, setTimer] = useState(() => initialTime());
+
+  const status = timer === 0 ? "Finished" : timer < 5 ? "Alert" : "Normal";
+  useEffect(() => {
+    const handleTimer = () => {
+      if (timer > 0) {
+        setTimer((prev) => prev - 1);
+      }
+    };
+    const timerInterval = setInterval(handleTimer, 1000);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [timer]);
   return (
     <Container>
-      <Clock>60</Clock>
+      <Clock status={status}>{timer}</Clock>
     </Container>
   );
 };
